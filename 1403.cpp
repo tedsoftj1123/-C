09236b5c6c085851d64af154596b03581d2d0ea2
tedsoftj1123 <tedsoftj1123@gmail.com>
2022-04-18 19:54:18 +0900

    [ADD] 1403

---
+++ 1403.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+	int n, num[1001];
+	scanf("%d", &n);
+	for(int i=0;i<n;i++){
+		scanf("%d", &num[i]);
+	}
+	for(int i=0;i<n;i++){
+		printf("%d\n", num[i]);
+	}
+	for(int i=0;i<n;i++){
+		printf("%d\n", num[i]);
+	}
+	return 0;
+}
+
