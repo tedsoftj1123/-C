2440af255ae5074c21084b6fdd116806b36f55f5
tedsoftj1123 <tedsoftj1123@gmail.com>
2022-04-18 19:53:39 +0900

    [ADD] 1402

---
+++ 별찍기.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	for (int i = 1; i < n * 2; i++) {
+		if (i < n) {
+			for (int j = 1; j < i; j++) {
+				printf(" ");
+			}
+			for (int j = 0; j <= (n * 2) - (i*2); j++) {
+				printf("*");
+			}
+		}
+		else {
+			for (int j = 1; j < n - (i-n); j++) {
+				printf(" ");
+			}
+			for (int j = ((i-n+1) * 2) - 1; j > 0; j--) {
+				printf("*");
+			}
+		}
+		printf("\n");
+	}
+}
+
+/*
+*****
+ ***
+  *
+ ***
+*****
+*/
---
+++ 별찍기2.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	for (int i = 1; i <= n * 2; i++) {
+		if (i <= n) {
+			for (int j = 0; j < n - i; j++) {
+				printf(" ");
+			}
+			for (int j = 0; j < i * 2 - 1; j++) {
+				printf("*");
+			}
+		}
+		else {
+			for (int j = i - n; j > 0; j--) {
+				printf(" ");
+			}
+			for (int j = (n * 2 - (i - n) * 2) - 1; j > 0; j--) {
+				printf("*");
+			}
+		}
+		printf("\n");
+	}
+	return 0;
+} 
+/*
+  *
+ ***
+*****
+ ***
+  *
+  */
